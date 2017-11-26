6a8bf998d26953ffcce0bfc60f4a6415e5eaa9bf
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-11-27 01:09:55 +0200

    initial commit with some drafts of code

---
+++ F_Sharp_Pie_Chart/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let convertStringRowToDataItem (stringRow:string) =
+    let cells = List.ofSeq (stringRow.Split ',')
+    match cells with
+    | lable::value::_ ->
+        let integerValue = Int32.Parse value
+        (lable, integerValue)
+    | _ -> failwith "Incorrect data format!"
+
+
+let rec processLinesToDataItemList lines =
+    match lines with
+    | [] -> []
+    | stringRow::tail ->
+        let dataItem = convertStringRowToDataItem stringRow
+        let rest = processLinesToDataItemList tail
+        dataItem::rest
+
+let rec countSum rows =
+   match rows with
+   | [] -> 0
+   | (_, n)::tail ->
+       let sumRest = countSum(tail)
+       n + sumRest
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
