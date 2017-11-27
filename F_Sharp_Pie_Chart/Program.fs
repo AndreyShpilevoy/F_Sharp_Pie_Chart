39b138fa0d9550cb04ead6f9fcb7c0622b48ed7a
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-11-28 00:15:52 +0200

    added read file functionality

--- F_Sharp_Pie_Chart/Program.fs
+++ F_Sharp_Pie_Chart/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.IO
 
 let convertStringRowToDataItem (stringRow:string) =
     let cells = List.ofSeq (stringRow.Split ',')
@@ -25,6 +26,16 @@ let rec countSum rows =
        n + sumRest
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    let lines = List.ofSeq(File.ReadAllLines(@"C:\data.csv"))
+    let data = processLinesToDataItemList(lines)
+    let sum = float(countSum(data))
+
+    for (lbl, num) in data do
+        let perc = int((float(num)) / sum * 100.0)
+        printfn "%-18s - %8d (%d%%)" lbl num perc
+ 
     0 // return an integer exit code
+
+
+//Asia, 44579000//Africa, 30065000//North America, 24256000//South America, 17819000//Antarctica, 13209000//Europe, 9938000//Australia/Oceania, 7687000
